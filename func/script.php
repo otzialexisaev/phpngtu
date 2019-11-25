256ecc2e89d522600219a730b1ff000179ebe286
usernamecheck <runyanavenue98@gmail.com>
2019-11-26 01:47:35 +0300

    вернул ядро в корень, все работает из коробки типа

--- func/script.php
+++ func/script.php
@@ -9,7 +9,7 @@ ini_set("xdebug.var_display_max_children", -1);
 ini_set("xdebug.var_display_max_data", -1);
 ini_set("xdebug.var_display_max_depth", -1);
 require_once "Core.php";
-require_once __DIR__ . "/../../designer/Designer.php";
+require_once __DIR__ . "/../designer/Designer.php";
 
 $core = new Core();
 $display = new Designer();
